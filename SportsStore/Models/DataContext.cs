8831e2c55c76e64df2beeb9906662775195a7e75
wxh0800montreal <wxh0800montreal@hotmail.com>
2019-06-13 08:44:06 -0400

    clear files

--- SportsStore/Models/DataContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace SportsStore.Models {
-    public class DataContext : DbContext {
-        public DataContext(DbContextOptions<DataContext> opts) : base(opts) {}
-        public DbSet<Product> Products {get; set;}
-    }
-}
